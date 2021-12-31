4e08dbfc160131b4a1c300393f2b63dd748e0eb1
LucasBruton <70657230+LucasBruton@users.noreply.github.com>
2021-12-31 10:47:14 +0100

    fixes (#3)

--- Code/src/Model/Start.java
+++ Code/src/Model/Start.java
@@ -2,12 +2,16 @@ package Model;
 
 import Vue.Fenetre;
 
+// Classe implémentant Runnable permettant de lancer la fenetre de l'application
 public class Start implements java.lang.Runnable{
+    // Accès au modèle 
     private KanagUT kanagUT;
-
+    // Constructeur de la classe
     public Start() {
         kanagUT = new KanagUT();
     }
+
+    // Lancement de la fenetre de l'application
     public void run(){
         new Fenetre(kanagUT);
 
